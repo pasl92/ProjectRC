a6c1a964c7d3c4c35809ab056b45648881c93324
Paweł Sałata <pawel.salata@microsoft.wsei.edu.pl>
2021-02-09 12:44:35 +0100

    add data to bd

---
+++ SQLQuery1.sql
@@ -0,0 +1,6 @@
+﻿insert into dbo.klienci VALUES
+('1','Pawel','Kowalski','578000111'),
+('2',null,null,'Jan','Wojcik','678000411'),
+('3',null,null,'Adam','Skrzat','738055511'),
+('4',null,null,'Damian','Wolny','838055444');
+
